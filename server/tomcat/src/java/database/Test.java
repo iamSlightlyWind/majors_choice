7654f53a96a48a264adb3f00e79bb2b2b467c1b4
iamSlightlyWind <70873548+iamSlightlyWind@users.noreply.github.com>
2024-05-19 15:50:31 +0700

    Tomcat server: Added basic classes

---
+++ server/tomcat/src/java/database/Test.java
@@ -0,0 +1,10 @@
+package database;
+
+// This class is used to test the Database class. It contains a main method so do what you will.
+
+public class Test {
+    public static void main(String[] args) {
+        Database database = new Database();
+        System.out.println(database.selectAll("users"));
+    }
+}
--- server/tomcat/src/java/main/User.java
+++ server/tomcat/src/java/main/User.java
@@ -1,5 +1,16 @@
 package main;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class User {
-    
+    public String username, password;
+    public String fullName, address, email, phoneNumber;
+    public int sex;
+    public LocalDate birthDate, joinDate;
+
+    public String formatedDate(LocalDate date) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        return date.format(formatter);
+    }
 }
