241af8171262f6921cde07e4157807cd7bde35a8
Phong Pham <70873548+iamSlightlyWind@users.noreply.github.com>
2024-05-20 18:24:23 +0700

    POC Passed: Docker Networking (#6)

--- server/tomcat/src/java/database/Test.java
+++ server/tomcat/src/java/database/Test.java
@@ -1,10 +1,36 @@
 package database;
 
-// This class is used to test the Database class. It contains a main method so do what you will.
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
-public class Test {
-    public static void main(String[] args) {
-        Database database = new Database();
-        System.out.println(database.selectAll("users"));
+public class Test extends HttpServlet {
+    Database database = new Database();
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+        
+        // Write the content of the system out to the response
+        response.getWriter().println(database.selectAll("users"));
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
     }
 }
