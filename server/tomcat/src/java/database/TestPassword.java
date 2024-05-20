4440884a423a41b55e7b09543508efa8699a2a03
iamSlightlyWind <70873548+iamSlightlyWind@users.noreply.github.com>
2024-05-20 17:55:44 +0700

    im running out of ideas

---
+++ server/tomcat/src/java/database/TestPassword.java
@@ -0,0 +1,36 @@
+package database;
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class TestPassword extends HttpServlet {
+    Database database = new Database();
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+        
+        // Write the content of the system out to the response
+        response.getWriter().println(System.getenv("SQLPASSWORD"));
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+}
