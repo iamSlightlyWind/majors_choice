4f640e456937b9ce41efe8a4c1ab60c4124b553f
Travis2101 <126550772+Travis2101@users.noreply.github.com>
2024-05-31 21:38:58 +0700

    Login google (#17)
    Untested change. Will test next commit

---
+++ server/tomcat/src/java/main/UserGoogle.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+
+/**
+ *
+ * @author PC
+ */
+public class UserGoogle {
+
+    private String id;
+    private String email;
+    private boolean verified_email;
+    private String picture;
+
+    public UserGoogle() {
+    }
+
+    public UserGoogle(String id, String email, boolean verified_email, String picture) {
+        this.id = id;
+        this.email = email;
+        this.verified_email = verified_email;
+        this.picture = picture;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isVerified_email() {
+        return verified_email;
+    }
+
+    public void setVerified_email(boolean verified_email) {
+        this.verified_email = verified_email;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    @Override
+    public String toString() {
+        return "UserGoogle{" + "id=" + id + ", email=" + email + ", verified_email=" + verified_email + ", picture=" + picture + '}';
+    }
+}
