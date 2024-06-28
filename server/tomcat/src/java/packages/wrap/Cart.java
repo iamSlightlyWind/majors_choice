704ccae64457271290520fadf339ba289eeae8bc
iamSlightlyWind <70873548+iamSlightlyWind@users.noreply.github.com>
2024-06-28 17:33:32 +0700

    Hardware Validation implemented

--- server/tomcat/src/java/packages/wrap/Cart.java
+++ server/tomcat/src/java/packages/wrap/Cart.java
@@ -19,6 +19,10 @@ public class Cart {
         this.quantities = new ArrayList<ProductCount>(cart.quantities);
     }
 
+    public void validate(){
+        Validation.validate(this);
+    }
+
     public void updateQuantity() {
         buyable = true;
 
