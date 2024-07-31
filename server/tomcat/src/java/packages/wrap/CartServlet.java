09960296322722fcd2ab3e076da519b3a3e3d62e
huy <huynkhe170654@fpt.edu.vn>
2024-07-31 21:08:41 +0700

    Coupon applied

--- server/tomcat/src/java/packages/wrap/CartServlet.java
+++ server/tomcat/src/java/packages/wrap/CartServlet.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Locale;
-
+import database.Database;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -79,37 +79,104 @@ public class CartServlet extends HttpServlet {
                 response.sendRedirect("/Cart");
                 break;
             case "checkout":
-                Cart tempCart = new Cart(currentUser.cart);
-                request.getSession().setAttribute("tempCart", tempCart);
-                currentUser.cart.getValidate();
-                currentUser.cart.clearCart();
-
-                request.setAttribute("cartPriceDouble", new DecimalFormat("#").format(tempCart.total));
-                request.setAttribute("cartPrice", String.format(Locale.US, "%,.2f", tempCart.total));
-                request.setAttribute("ProductCount", (ArrayList<ProductCount>) tempCart.quantities);
-                request.setAttribute("validation", tempCart.getValidate());
-                request.setAttribute("user", currentUser);
-
-                request.setAttribute("socketMatch", Validation.socketMatch());
-                request.setAttribute("graphicsOutput", Validation.graphicsOutput());
-                request.setAttribute("ramType", Validation.ramType());
-                request.setAttribute("ramMatching", Validation.ramMatching());
-                request.setAttribute("ramSpeed", Validation.ramSpeed());
-                request.setAttribute("ramCapacity", Validation.ramCapacity());
-                request.setAttribute("powerBudget", Validation.powerBudget());
-
-                request.getRequestDispatcher("/cart/checkout.jsp").forward(request, response);
+                request.getSession().removeAttribute("DiscountedCart");
+                checkout(request, response, currentUser, true);
                 break;
             case "restore":
                 Cart restoreCart = (Cart) request.getSession().getAttribute("tempCart");
                 currentUser.cart = new Cart(restoreCart);
                 currentUser.cart.updateCart();
-
                 response.sendRedirect("/Cart");
+                break;
+            case "applyCoupon":
+                applyCoupon(request, response, currentUser);
+                checkout(request, response, currentUser, false);
                 break;
         }
 
         request.getSession().setAttribute("userObject", currentUser);
+    }
+
+    protected void applyCoupon(HttpServletRequest request, HttpServletResponse response, User currentUser)
+            throws ServletException, IOException {
+
+        int status = Database.reserveCoupon(request.getParameter("couponCode"));
+        String error = "";
+
+        switch (status) {
+            case -1:
+                error = "Coupon not found";
+                break;
+            case -2:
+                error = "Coupon expired";
+                break;
+            case -3:
+                error = "Coupon out of uses";
+                break;
+        }
+
+        if (!error.equals("")) {
+            request.setAttribute("couponStatus", error);
+            request.getSession().setAttribute("DiscountedCart", request.getSession().getAttribute("tempCart"));
+            request.getSession().removeAttribute("DiscountedCart");
+            return;
+        } else {
+            Coupon coupon = Database.getCouponNumbers(request.getParameter("couponCode"));
+            Cart tempCart = new Cart((Cart) request.getSession().getAttribute("tempCart"));
+            tempCart.updateQuantityTemp();
+            if (tempCart.total * coupon.discount > coupon.maxDiscount) {
+                coupon.discount = coupon.maxDiscount / tempCart.total;
+            }
+
+            if (tempCart.total < coupon.minPurchase) {
+                request.setAttribute("couponStatus", "Minimum purchase (" + coupon.minPurchase + "VNĐ) not met");
+            } else {
+                adjustPrice(tempCart, coupon.discount);
+            }
+
+            request.getSession().setAttribute("DiscountedCart", tempCart);
+            request.setAttribute("couponStatus",
+                    "Coupon applied: " + String.format("%.2f", coupon.discount * 100) + "% discount");
+            request.setAttribute("appliedCoupon", coupon.code);
+        }
+    }
+
+    protected void adjustPrice(Cart tempCart, double discountPercentage) {
+        for (ProductCount product : tempCart.quantities) {
+            product.pricePer *= 1 - discountPercentage;
+        }
+        tempCart.updateTotal();
+    }
+
+    protected void checkout(HttpServletRequest request, HttpServletResponse response, User currentUser,
+            Boolean hideCart)
+            throws ServletException, IOException {
+        Cart tempCart = null;
+
+        if (hideCart) {
+            tempCart = new Cart(currentUser.cart);
+            request.getSession().setAttribute("tempCart", tempCart);
+            currentUser.cart.getValidate();
+            currentUser.cart.clearCart();
+        } else {
+            tempCart = (Cart) request.getSession().getAttribute("DiscountedCart");
+        }
+
+        request.setAttribute("cartPriceDouble", new DecimalFormat("#").format(tempCart.total));
+        request.setAttribute("cartPrice", String.format(Locale.US, "%,.2f", tempCart.total));
+        request.setAttribute("ProductCount", (ArrayList<ProductCount>) tempCart.quantities);
+        request.setAttribute("validation", tempCart.getValidate());
+        request.setAttribute("user", currentUser);
+
+        request.setAttribute("socketMatch", Validation.socketMatch());
+        request.setAttribute("graphicsOutput", Validation.graphicsOutput());
+        request.setAttribute("ramType", Validation.ramType());
+        request.setAttribute("ramMatching", Validation.ramMatching());
+        request.setAttribute("ramSpeed", Validation.ramSpeed());
+        request.setAttribute("ramCapacity", Validation.ramCapacity());
+        request.setAttribute("powerBudget", Validation.powerBudget());
+
+        request.getRequestDispatcher("/cart/checkout.jsp").forward(request, response);
     }
 
     @Override
